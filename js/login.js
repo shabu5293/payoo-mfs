a69b4a2c35aa69d8f7ebd7954565225142ca0ad8
shabu529393 <mshoque5293@gmail.com>
2024-11-30 23:31:28 +0600

    Done login form and transfer to homepage

--- js/login.js
+++ js/login.js
@@ -5,7 +5,18 @@ document.getElementById('button-login').addEventListener('click', function(event
 
     // step -2: prevent default behavior ( prevent reloading browser)
     event.preventDefault();
-    // step -3: get the phone number
+    // step -3: get the phone number and pin number
     const phoneNumber = document.getElementById('phone-number').value;
-    
+    const pinNumber = document.getElementById('pin-number').value;
+
+    // Step -4: Validate phone and pin
+    // This is temporary. Not proper way
+    if(phoneNumber === '5' && pinNumber === '1234'){
+        console.log('You are loggeed in');
+        window.location.href = '/home.html';
+        // Step -5: Allow user to use the website
+    }
+    else{
+        alert ('Did not match');
+    }
 })
